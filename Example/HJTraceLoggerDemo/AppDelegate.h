49a2c8bdba3b31d3f7a3c0df7dadb5a601e28823
navy <lzxy169@gmail.com>
2021-08-16 17:10:50 +0800

    update HJTraceLogger

---
+++ Example/HJTraceLoggerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HJTraceLoggerDemo
+//
+//  Created by navy on 2021/8/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
